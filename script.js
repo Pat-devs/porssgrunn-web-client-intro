f8315c769506cebdc5cf8e54b31a5b181f992f45
pat-devs <patryk.kodehode@gmail.com>
2024-12-03 08:16:18 +0100

    Revert "external api usage example"
    This reverts commit eb1790a976100e1c3915359a573db7b41c3b6993.

--- script.js
+++ script.js
@@ -16,57 +16,81 @@ function displayBookInfo(bookObject) {
         `
 }
 
-/* 
+let libraryArray = [
+    {
+        title: "Foundation"
+    },
+    {
+        title: "Martian"
+    },
+    {
+        title: "1984"
+    }
+]
+
 let libraryData = '[{"title":"Foundation"},{"title":"Martian"}]'
 
 let convertedLibraryData =  JSON.parse(libraryData)
 
 
-//console.log(convertedLibraryData[0].title)
+//console.log(convertedLibraryData)
 
 
+const arrayOfObjects = [
+    {
+      id: 1,
+      name: "Alice",
+      age: 25,
+      occupation: "Engineer",
+      stuff: `doSomething() {
+        console.log("Working")
+      }`
+    },
+    {
+      id: 2,
+      name: "Bob",
+      age: 30,
+      occupation: "Designer",
+    },
+    {
+      id: 3,
+      name: "Charlie",
+      age: 28,
+      occupation: "Teacher",
+    },
+    {
+      id: 4,
+      name: "Diana",
+      age: 22,
+      occupation: "Developer",
+    },
+  ];
 
-for (let book of convertedLibraryData) {
+
+  console.log(arrayOfObjects)
+  
+
+  let convertedArrayOfObjects = JSON.stringify(arrayOfObjects)
+
+  console.log(convertedArrayOfObjects)
+
+  console.log( JSON.parse(convertedArrayOfObjects))
+
+
+  convertedArrayOfObjects[0].doSomething()
+
+
+/* for (let book of libraryArray) {
     console.log(book)
     displayBookInfo(book)
-}
-
-//console.log(libraryData) */
-
-
-// Get some data from a url 
-
-// Display the given book in the booksContainerElement
-function displayUserInfo(userObject) {
-
-    let title = userObject.name
-
-    booksContainerElement.innerHTML += 
-        `<div class="book-container">
-            <h2>Title: ${title}</h2>
-        </div>
-        `
-}
-
-// Using the most modern "async function"
+} */
 
 
 
 
 
-// https://jsonplaceholder.typicode.com/users
-async function getData() {
-    // make a fetch-request to an API url, and store the response in a variable
-    let response = await fetch("https://jsonplaceholder.typicode.com/users")
-    // get the response data:
-    let data = await response.text()
 
-    //console.log(data)
 
-    for (let user of data) {
-        console.log(user)
-        displayUserInfo(user)
-    }
-}
 
-getData()
+
+//console.log(libraryData)
