3f358997e957394ddee176828c7ecb13b982cb23
pat-devs <patryk.kodehode@gmail.com>
2024-12-03 10:57:07 +0100

    added comments

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 let booksContainerElement = document.getElementById("books-container")
 let borrowBookInfoContainerElement = document.getElementById("borrow-book-info-container")
 
+let borrowBookInputElement = document.getElementById("borrow-book-input-name")
+let borrowBookButtonElement = document.getElementById("borrow-book-button")
+
+// the readInput click event handler function invokes  several functions:
+// 1. borrowBook
+//    which invokes displayBookLoanRequestInfo
+//                  which displays book info on the page
+function readInput() {
+    
+    borrowBook(borrowBookInputElement.value)
+}
+
+
+borrowBookButtonElement.onclick = readInput
+
+// <button onclick="count()">
+
 // Display the given book in the booksContainerElement
 function displayBookInfo(bookObject) {
 
